6d11fdc95c824d49c3a3180bcff8673d6e06788d
brycejjacks <brycejjacks@gmail.com>
2026-05-30 10:40:55 -0700

    feat(init): scaffold

---
+++ backend/src/main/java/com/calcguide/CalcGuideApplication.java
@@ -0,0 +1,11 @@
+package com.calcguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalcGuideApplication.class, args);
+    }
+}
